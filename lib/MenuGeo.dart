e5617334b5e66053ad86180a06fcb97b7a34a013
JpBllack <joaogomes@unitins.br>
2024-12-19 09:16:14 -0300

    alterações em Menu do Jogo

--- lib/MenuGeo.dart
+++ lib/MenuGeo.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'PaisesPage.dart';
+import 'bandeiras.dart';
+import 'estados.dart';
 
 class MenuGeo extends StatelessWidget {
   const MenuGeo({super.key});
@@ -22,13 +25,17 @@ class MenuGeo extends StatelessWidget {
         ),
         child: Center(
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.center, // Centraliza na vertical
+            mainAxisAlignment:
+                MainAxisAlignment.center, // Centraliza na vertical
             children: [
-              _buildSquare('assets/images/bandeiras.png'),
+              _buildSquare(context, 'assets/images/bandeiras.png', 'Bandeiras',
+                  'Bandeira'),
               SizedBox(width: 80), // Espaço entre os quadrados
-              _buildSquare('assets/images/estados.png'), // Quadrado 2
+              _buildSquare(context, 'assets/images/estados.png', 'Estados',
+                  'Estados'), // Quadrado 2
               SizedBox(width: 80), // Espaço entre os quadrados
-              _buildSquare('assets/images/paises.png'), // Quadrado 3
+              _buildSquare(context, 'assets/images/paises.png', 'Países',
+                  'Países'), // Quadrado 3
             ],
           ),
         ),
@@ -36,19 +43,59 @@ class MenuGeo extends StatelessWidget {
     );
   }
 
-  // Função para criar o quadrado
-  Widget _buildSquare(String imagePath) {
-    return Container(
-      width: 200,
-      height: 200,
-      decoration: BoxDecoration(
-        color: Colors.grey,
-        borderRadius: BorderRadius.circular(15),
+  // Função para criar o quadrado com navegação
+ Widget _buildSquare(
+    BuildContext context, String imagePath, String routeName, String nome) {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      GestureDetector(
+        onTap: () {
+          if (routeName == 'Bandeiras') {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => BandeirasPage(),
+              ),
+            );
+          } else if (routeName == 'Estados') {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => EstadosPage()),
+            );
+          } else if (routeName == 'Países') {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => PaisesPage()),
+            );
+          }
+        },
+        child: Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+            color: Colors.grey,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(5),
+            child: Image.asset(
+              imagePath,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
       ),
-      child: Padding(
-        padding: EdgeInsets.all(5),
-        child: Image.asset(imagePath, fit: BoxFit.cover,),
+      SizedBox(height: 10), // Espaço entre o quadrado e o texto
+      Text(
+        nome,
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),
       ),
-    );
-  }
+    ],
+  );
 }
+ }
